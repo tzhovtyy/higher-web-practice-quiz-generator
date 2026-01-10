5d2c62fe3a5182702acd3ef8f7bfb89429a3ba28
Timofei <tzzzhovtyy@gmail.com>
2026-01-11 01:02:59 +1000

    chore: vite setup

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,23 @@
-// Напишите конфиг
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  root: ".",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        quizzes: resolve(__dirname, "quizzes.html"),
+        quiz: resolve(__dirname, "quiz.html"),
+      },
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        chunkFileNames: "js/[name].js",
+        entryFileNames: "js/[name].js",
+      },
+    },
+  },
+  server: {
+    open: "/index.html",
+  },
+});
